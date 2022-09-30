fa18d28cbb81b2a07a2e08d97920cfec7c8dbb56
Andrx4s <49282969+Andrx4s@users.noreply.github.com>
2022-09-30 13:56:28 +0500

    Update ProductController.php

--- laravel9/app/Http/Controllers/ProductController.php
+++ laravel9/app/Http/Controllers/ProductController.php
@@ -2,58 +2,70 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Http\Requests\Admin\Pruduct\ProductCreateValidation;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Product $product)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
+=======
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        $products = Product::paginate(15);
+        return view('admin.product.index', compact('products'));
+    }
+
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function create()
+    {
+        return view('admin.product.createOrUpdate');
+    }
+
+    /**
+     * @param ProductCreateValidation $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(ProductCreateValidation $request)
+    {
+        $validate = $request->validated();
+        unset($validate['photo_file']);
+        # public/asd.jpg
+        $photo = $request->file('photo_file')->store('public');
+        # Explode => / => public/asd,jpg => ['public', 'asd.jpg']
+        $validate['photo'] = explode('/', $photo)[1];
+
+        Product::create($validate);
+        return back()->with(['success' => true]);
+    }
+
+    /**
+     * @param Product $product
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function show(Product $product)
+    {
+        $breadcrumbs = [
+            ['routeName' => 'welcome', 'name' => 'Главная страница'],
+            ['routeName' => 'admin.product.index', 'name' => 'Все продукты'],
+            ['name'=> $product->name]
+        ];
+        return view('admin.product.show', compact('product', 'breadcrumbs'));
+    }
+
+    /**
+     * @param Product $product
+     * @return void
+>>>>>>> b955ab1 (Version 1.0.1)
      */
     public function edit(Product $product)
     {
@@ -61,11 +73,17 @@ class ProductController extends Controller
     }
 
     /**
+<<<<<<< HEAD
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
+=======
+     * @param Request $request
+     * @param Product $product
+     * @return void
+>>>>>>> b955ab1 (Version 1.0.1)
      */
     public function update(Request $request, Product $product)
     {
@@ -73,10 +91,15 @@ class ProductController extends Controller
     }
 
     /**
+<<<<<<< HEAD
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
+=======
+     * @param Product $product
+     * @return void
+>>>>>>> b955ab1 (Version 1.0.1)
      */
     public function destroy(Product $product)
     {
